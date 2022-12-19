9437b890bd225cb20e1ea7997bfb1e86dcb281b5
d1mpi <d1mpi.developer@gmail.com>
2022-12-19 10:20:00 +0300

    Initial commit

--- cache.go
+++ cache.go
@@ -1,5 +1,7 @@
 package cache
 
+import "errors"
+
 type Cache interface {
 	New()
 }
@@ -13,14 +15,30 @@ func New() Data {
 	return Data{newCacheObject}
 }
 
-func (cache Data) Set(key string, value interface{}) {
+func (cache Data) Set(key string, value interface{}) error {
+	if len(key) == 0 {
+		return errors.New("the key length is zero")
+	} else if value == nil {
+		return errors.New("the value length is zero")
+	}
+
 	cache.cache[key] = value
+	return nil
 }
 
-func (cache Data) Get(key string) interface{} {
-	return cache.cache[key]
+func (cache Data) Get(key string) (interface{}, error) {
+	if len(key) == 0 {
+		return nil, errors.New("the key length is zero")
+	}
+
+	return cache.cache[key], nil
 }
 
-func (cache Data) Delete(key string) {
+func (cache Data) Delete(key string) error {
+	if len(key) == 0 {
+		return errors.New("the key length is zero")
+	}
+
 	delete(cache.cache, key)
+	return nil
 }
